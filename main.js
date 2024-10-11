7c5b3ca8c75528be7aa0c05f295233baf57e5c57
burning-monk <18663097+burning-monk@users.noreply.github.com>
2024-10-11 18:15:25 +0200

    adapted column log structure

---
+++ main.js
@@ -0,0 +1,4 @@
+const aimList = document.querySelector('.aim-tracker_')
+const labelList = document.querySelector('.aim-tracker__days-label')
+const entriesList = document.querySelector('');
+const reviewList = document.querySelector('.aim-tracker__review-list')
