453546d5ca7b596b90e9438c8e0714179730b0fa
Jan-Dev0 <18663097+Jan-Dev0@users.noreply.github.com>
2024-10-12 13:29:35 +0200

    feat: add function to dynamically create aim input fields

--- main.js
+++ main.js
@@ -1,4 +1,35 @@
-const aimList = document.querySelector('.aim-tracker_')
-const labelList = document.querySelector('.aim-tracker__days-label')
-const entriesList = document.querySelector('');
-const reviewList = document.querySelector('.aim-tracker__review-list')
+const aimList = document.querySelector(".aim-tracker__aim-list");
+const labelList = document.querySelector(".aim-tracker__days-label");
+const entriesList = document.querySelector(".aim-tracker__entries");
+const reviewList = document.querySelector(".aim-tracker__review-list");
+const leftArrow = document.querySelector(".aim-tracker__arrow--left");
+const rightArrow = document.querySelector(".aim-tracker__arrow--right");
+
+const createAims = () => {
+  const div = document.createElement("div");
+  const aimInput = document.createElement("input");
+  const timeInput = document.createElement("input");
+
+  div.classList.add("aim-tracker__aim");
+
+  aimInput.type = "text";
+  aimInput.placeholder = "Ziel";
+  aimInput.classList.add("aim-tracker__aim-input");
+
+  timeInput.type = "text";
+  timeInput.placeholder = "Zeit";
+  timeInput.classList.add("aim-tracker__time-input");
+  div.append(aimInput, timeInput);
+  aimList.append(div);
+};
+
+const createLabels = () => {};
+
+const createEntries = () => {};
+
+const createReview = () => {};
+
+for (let i = 0; i < 5; i++) {
+  createAims();
+}
+
