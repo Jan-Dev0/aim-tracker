09cc770723d08d9775d06e048b7416b7c370eb0e
burning-monk <18663097+burning-monk@users.noreply.github.com>
2024-10-12 15:45:54 +0200

    feat: add functions for  add log labels, log entries and review entries

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 const aimList = document.querySelector(".aim-tracker__aim-list");
 const labelList = document.querySelector(".aim-tracker__days-label");
-const entriesList = document.querySelector(".aim-tracker__entries");
+const entriesList = document.querySelector(".aim-tracker__entries-list");
 const reviewList = document.querySelector(".aim-tracker__review-list");
 const leftArrow = document.querySelector(".aim-tracker__arrow--left");
 const rightArrow = document.querySelector(".aim-tracker__arrow--right");
+const entriesContainer = document.querySelector(
+  ".aim-tracker__entries-container"
+);
 
-const createAims = () => {
+const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth();
+const lastDayOfMonth = new Date(year, month + 1, 0).getDate();
+const scrollAmount = 250;
+
+const createAimEntry = () => {
   const div = document.createElement("div");
   const aimInput = document.createElement("input");
   const timeInput = document.createElement("input");
@@ -23,13 +32,66 @@ const createAims = () => {
   aimList.append(div);
 };
 
-const createLabels = () => {};
+const createReviewEntry = () => {
+  const entry = document.createElement("div");
+  const input = document.createElement("input");
 
-const createEntries = () => {};
+  entry.classList.add("aim-tracker__review-list");
+  input.type = "text";
+  input.placeholder = "Auswertung";
+  input.classList.add("aim-tracker__review-input");
 
-const createReview = () => {};
+  entry.append(input);
+  reviewList.append(entry);
+};
+
+const createLogLabel = () => {
+  for (let i = 1; i <= lastDayOfMonth; i++) {
+    const label = document.createElement("div");
+    label.classList.add("aim-tracker__day");
+    label.textContent = `${i}`;
+    labelList.append(label);
+  }
+};
+
+const createLogEntry = () => {
+  const listRow = document.createElement("div");
+  listRow.classList.add("aim-tracker__entries");
+
+  for (let i = 1; i <= lastDayOfMonth; i++) {
+    const entry = document.createElement("div");
+    const input = document.createElement("input");
+
+    entry.classList.add("aim-tracker__entry");
+    input.type = "text";
+    input.classList.add("aim-tracker__entry-input");
+
+    entry.append(input);
+    listRow.append(entry);
+  }
+  entriesList.append(listRow);
+};
 
 for (let i = 0; i < 5; i++) {
-  createAims();
+  createAimEntry();
+  createReviewEntry();
+  createLogEntry();
 }
 
+createLogLabel();
+
+leftArrow.addEventListener("click", () => {
+  console.log("left");
+  entriesContainer.scrollBy({
+    left: -scrollAmount,
+    behavior: "smooth",
+  });
+});
+
+rightArrow.addEventListener("click", () => {
+  console.log("right");
+  entriesContainer.scrollBy({
+    left: scrollAmount,
+    behavior: "smooth",
+  });
+});
